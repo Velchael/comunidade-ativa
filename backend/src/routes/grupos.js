03bb7b8e59a2596890a5ce030ba1c277564bc11c
Velchael Mendez <velchael@gmail.com>
2026-05-28 18:19:47 -0300

    fase 4.2 activar autorizacion hibrida en grupos

--- backend/src/routes/grupos.js
+++ backend/src/routes/grupos.js
@@ -2,7 +2,23 @@ const express = require('express');
 const router = express.Router();
 const { verificarToken } = require('../middleware/authMiddleware');
 const gruposController = require('../controllers/gruposController');
-const permisoEditarEliminar = require('../middleware/permisoEditarEliminar');
+const verificarRolComunidad = require('../middleware/verificarRolComunidad');
+const db = require('../models');
+
+const getGrupoComunidadId = async (req) => {
+  const grupo = await db.GrupoActivo.findByPk(req.params.id, {
+    attributes: ['id', 'comunidad_id']
+  });
+
+  return grupo?.comunidad_id;
+};
+
+const allowGrupoAdmins = verificarRolComunidad({
+  rolesPermitidos: ['admin_total', 'admin_basic'],
+  getComunidadId: getGrupoComunidadId,
+  permitirAdminTotalGlobal: true
+});
+
 // 📍 Rutas para grupos
 
 // Listar todos los grupos (con reglas internas en el controller)
@@ -17,7 +33,7 @@ router.get('/:id', verificarToken, gruposController.obtenerGrupo);
 // Crear un grupo (cualquier usuario logado puede crear según sus reglas)
 router.post('/', verificarToken, gruposController.crearGrupo);
 
-router.put('/:id', verificarToken, permisoEditarEliminar, gruposController.actualizarGrupo);
-router.delete('/:id', verificarToken, permisoEditarEliminar, gruposController.eliminarGrupo);
+router.put('/:id', verificarToken, allowGrupoAdmins, gruposController.actualizarGrupo);
+router.delete('/:id', verificarToken, allowGrupoAdmins, gruposController.eliminarGrupo);
 
 module.exports = router;
